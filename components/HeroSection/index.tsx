391b24e49a3e417de6f70f77868db9354419461a
Aniruddha Bagal <bagalaniruddha@gmail.com>
2023-11-03 20:34:13 +0530

    feat: hero section complete

--- components/HeroSection/index.tsx
+++ components/HeroSection/index.tsx
@@ -48,7 +48,10 @@
 import React from "react";
 import { stylesConfig } from "@/utils/functions";
 import styles from "./styles.module.scss";
-import { Typography } from "@/library";
+import { Button, Typography } from "@/library";
+import rocketImg from "@/public/icons/rocket.svg";
+import Image from "next/image";
+import { BsArrowRightShort } from "react-icons/bs";
 
 const classes = stylesConfig(styles, "hero-section");
 
@@ -56,64 +59,172 @@ const HeroSection = () => {
 	return (
 		<>
 			<div className={classes("")}>
-				<div className={classes("-heading")}>
-					<div className={classes("-heading-title-1")}>
-						<Typography
-							family="red-hat-display"
-							size="lg"
-							weight="bold"
-							as="h3"
-							className={classes("-heading-title-1-text")}
-						>
-							Learn in-demand
-						</Typography>
+				<div className={classes("-top-gradient")}></div>
+
+				<div className={classes("-content-container")}>
+					<div className={classes("-heading-container")}>
+						<div className={classes("-heading")}>
+							<div className={classes("-heading-title-1")}>
+								<Typography
+									family="red-hat-display"
+									size="lg"
+									weight="bold"
+									as="h3"
+									className={classes("-heading-title-1-text")}
+								>
+									Learn in-demand
+								</Typography>
+							</div>
+							<div className={classes("-heading-title-2")}>
+								<Typography
+									family="red-hat-display"
+									size="xxxl"
+									weight="bold"
+									as="span"
+									className={classes(
+										"-heading-title-2-text-1"
+									)}
+								>
+									AI Skills
+								</Typography>
+								<Typography
+									family="red-hat-display"
+									size="xxxl"
+									weight="bold"
+									as="span"
+									className={classes(
+										"-heading-title-2-text-2"
+									)}
+								>
+									{" "}
+									from
+								</Typography>
+							</div>
+							<div className={classes("-heading-title-3")}>
+								<Typography
+									family="red-hat-display"
+									size="xxxl"
+									weight="bold"
+									as="span"
+									className={classes("-heading-title-3-text")}
+								>
+									Industry Experts
+								</Typography>
+							</div>
+						</div>
+						<div className={classes("-heading-subtitle")}>
+							<Typography
+								family="red-hat-text"
+								size="s"
+								weight="medium"
+								as="p"
+								className={classes("-heading-subtitle-text")}
+							>
+								Accelerate your career with our online courses
+								in leveraging{" "}
+								<Typography
+									family="red-hat-text"
+									size="s"
+									weight="medium"
+									as="span"
+									className={classes(
+										"-heading-subtitle-text-chatgpt"
+									)}
+								>
+									chatGPT
+								</Typography>{" "}
+								to become data scientist
+							</Typography>
+						</div>
 					</div>
-					<div className={classes("-heading-title-2")}>
-						<Typography
-							family="red-hat-display"
-							size="xxxl"
-							weight="bold"
-							as="span"
-							className={classes("-heading-title-2-text-1")}
-						>
-							AI Skills
-						</Typography>
-						<Typography
-							family="red-hat-display"
-							size="xxxl"
-							weight="bold"
-							as="span"
-							className={classes("-heading-title-2-text-2")}
-						>
-							{" "}
-							from
-						</Typography>
-					</div>
-					<div className={classes("-heading-title-3")}>
-						<Typography
-							family="red-hat-display"
-							size="xxxl"
-							weight="bold"
-							as="span"
-							className={classes("-heading-title-3-text")}
-						>
-							Industry Experts
-						</Typography>
+
+					<div className={classes("-domain-carousal-container")}>
+						<div className={classes("-carousal-items")}>
+							<Typography
+								family="red-hat-display"
+								size="xxxl"
+								weight="bold"
+								as="div"
+								className={classes("-carousal-item-1")}
+							>
+								ChatGPT
+							</Typography>
+							<Typography
+								family="red-hat-display"
+								size="xxxl"
+								weight="bold"
+								as="div"
+								className={classes("-carousal-item-1")}
+							>
+								LLM
+							</Typography>
+							<Typography
+								family="red-hat-display"
+								size="xxxl"
+								weight="bold"
+								as="div"
+								className={classes("-carousal-item-1")}
+							>
+								NLP
+							</Typography>
+							<Typography
+								family="red-hat-display"
+								size="xxxl"
+								weight="bold"
+								as="div"
+								className={classes("-carousal-item-1")}
+							>
+								Data Science
+							</Typography>
+							<Typography
+								family="red-hat-display"
+								size="xxxl"
+								weight="bold"
+								as="div"
+								className={classes("-carousal-item-1")}
+							>
+								Python
+							</Typography>
+							<Typography
+								family="red-hat-display"
+								size="xxxl"
+								weight="bold"
+								as="div"
+								className={classes("-carousal-item-1")}
+							>
+								UI/UX Design
+							</Typography>
+						</div>
 					</div>
 				</div>
-				<div className={classes("-heading-subtitle")}>
-					<Typography
-						family="red-hat-text"
-						size="s"
-						weight="medium"
-						as="p"
-						className={classes("-heading-subtitle-text")}
-					>
-						Accelerate your career with our online courses in
-						leveraging chatGPT to become data scientist
-					</Typography>
-				</div>
-				<div className={classes("-top-gradient")}></div>{" "}
+				<Button
+					size="medium"
+					theme="outlined"
+					className={classes("-launch-career-btn")}
+				>
+					<Image src={rocketImg} alt="rocket" />
+					<div className={classes("-btn-desc")}>
+						<Typography
+							family="red-hat-display"
+							size="s"
+							weight="bold"
+							as="div"
+							className={classes("-btn-text-1")}
+						>
+							Launch your career
+						</Typography>
+						<Typography
+							family="red-hat-text"
+							size="s"
+							weight="medium"
+							as="div"
+							className={classes("-btn-text-2")}
+						>
+							Unlock Opportunities and Achieve Your Goals
+						</Typography>
+					</div>
+					<BsArrowRightShort className={classes("-btn-arrow")} />
+				</Button>
 			</div>
 		</>
 	);
