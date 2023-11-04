2389021dd5609ba30536384f39cc5bcc4ce3a354
Aniruddha Bagal <bagalaniruddha@gmail.com>
2023-11-05 03:36:13 +0530

    feat: started why-us section

---
+++ components/WhyUsContainer/WhyUsContainer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./styles.module.scss";
+import { stylesConfig } from "@/utils/functions";
+import { Typography } from "@/library";
+
+const classes = stylesConfig(styles, "why-us-container");
+
+const WhyUsContainer = () => {
+	return <div>WhyUsContainer</div>;
+};
+
+export default WhyUsContainer;
