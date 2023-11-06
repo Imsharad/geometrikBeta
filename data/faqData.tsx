1b57bef3f8ad2e0ef2eb71f251739bf4baec674c
Aniruddha Bagal <bagalaniruddha@gmail.com>
2023-11-06 17:25:26 +0530

    feat: updated faq question

--- data/faqData.tsx
+++ data/faqData.tsx
@@ -4,31 +4,32 @@ export const faqs: {
 	answer: string | React.ReactElement;
 }[] = [
 	{
-		question: "What courses do you offer?",
+		question: "Are there any pre-requisites required for this course?",
 		answer: "We offer a wide range of courses in various domains, including programming, data science, business, and more. You can explore our course catalog to see the full list of offerings.",
 	},
 	{
-		question: "How do I enroll in a course?",
+		question: "How are the sessions conducted?",
 		answer: "To enroll in a course, simply browse our course catalog, select the course you're interested in, and click the 'Enroll' button. Follow the on-screen instructions to complete the enrollment process.",
 	},
 	{
-		question: "Are the courses self-paced?",
+		question:
+			"I have a full-time job. Will I get enough time to take this program?",
 		answer: "Yes, most of our courses are designed to be self-paced, allowing you to study at your own convenience. However, some courses may have specific start and end dates.",
 	},
 	{
-		question: "Do you offer certificates upon course completion?",
+		question: "How do mock interviews function?",
 		answer: "Yes, we provide certificates of completion for most courses. These certificates can be a valuable addition to your resume and professional profile.",
 	},
 	{
-		question: "How can I pay for a course?",
+		question: "How are the sessions conducted?",
 		answer: "We offer multiple payment options, including credit/debit cards, PayPal, and more. You can choose your preferred payment method during the enrollment process.",
 	},
 	{
-		question: "Is there any financial assistance available for courses?",
+		question: "Are there any pre-requisites required for this course?",
 		answer: "Yes, we offer financial assistance and scholarships for eligible students. Please visit our financial aid page for more information and application details.",
 	},
 	{
-		question: "Can I access course materials on mobile devices?",
+		question: "How do mock interviews function?",
 		answer: "Absolutely! Our platform is mobile-friendly, and you can access course materials on your smartphone or tablet using our mobile app or a web browser.",
 	},
 ];
