95f2f054accdcf1b2812564d9df738c24db3e0dc
Aniruddha Bagal <bagalaniruddha@gmail.com>
2023-11-20 12:34:05 +0530

    feat: configured seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,19 +8,40 @@ import Footer from "@/components/Footer";
 import FaqContainer from "@/components/FaqContainer/FaqContainer";
 import { stylesConfig } from "@/utils/functions";
 import styles from "@/styles/pages/Home.module.scss";
+import Seo from "@/layouts/Seo";
 
 const classes = stylesConfig(styles, "home");
 
 const HomePage: React.FC = () => (
-	<main className={classes("")}>
-		<HeroSection />
-		<CoursesContainer />
-		<WhyUsContainer />
-		<TestimonialsContainer />
-		<ThreadsContainer />
-		<FaqContainer />
-		<Footer />
-	</main>
+	<>
+		<Seo
+			title="Growth Rocket"
+			description="Accelerate your career with our online courses in leveraging chatGPT to become data scientist"
+			author="opengig"
+			canonical="https://growthrocket.ai/"
+			icons={[
+				{
+					rel: "icon",
+					href: "/favicon.ico",
+				},
+				{
+					rel: "apple-touch-icon",
+					href: "/apple-touch-icon.png",
+					sizes: "180x180",
+				},
+			]}
+			siteName="Growth Rocket"
+		/>
+		<main className={classes("")}>
+			<HeroSection />
+			<CoursesContainer />
+			<WhyUsContainer />
+			<TestimonialsContainer />
+			<ThreadsContainer />
+			<FaqContainer />
+			<Footer />
+		</main>
+	</>
 );
 
 export default HomePage;
